4ce16cd075696a6489f513123fe559c3657e71f2
mampiononavoahary <hei.zoarisoa@gmail.com>
2025-02-12 19:04:09 +0300

    feat: add service of firebase messaging

---
+++ android/app/src/main/java/com/notifcatioproject/FireBaseMessaging.kt
@@ -0,0 +1,96 @@
+package com.notifcatioproject
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FireBaseMessaging: FirebaseMessagingService(){
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "Message received from: ${remoteMessage.from}")
+
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+
+            if (isLongRunningJob()) {
+                scheduleJob()
+            } else {
+                handleNow()
+            }
+        }
+
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            showNotification(it.title, it.body)
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+        sendRegistrationToServer(token)
+    }
+
+    private fun isLongRunningJob() = true
+
+    private fun scheduleJob() {
+        val work = OneTimeWorkRequest.Builder(MyWorker::class.java).build()
+        WorkManager.getInstance(this).beginWith(work).enqueue()
+    }
+
+    private fun handleNow() {
+        Log.d(TAG, "Short lived task is done.")
+    }
+
+    private fun sendRegistrationToServer(token: String?) {
+        Log.d(TAG, "sendRegistrationTokenToServer($token)")
+    }
+
+    private fun showNotification(title: String?, message: String?) {
+        val requestCode = 0
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            requestCode,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val channelId = "default_channel_id"
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Default Channel",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_stat_ic_notification) // Utilisation de ic_stat_notification
+            .setContentTitle(title ?: "Notification")
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+            .setContentIntent(pendingIntent)
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMsgService"
+    }
+}
+
