2752da9373f113a71293d534491693a5bdc90054
mampiononavoahary <hei.zoarisoa@gmail.com>
2025-02-12 19:07:55 +0300

    feat: add goesBackground

---
+++ android/app/src/main/java/com/notifcatioproject/GoesBackround.kt
@@ -0,0 +1,18 @@
+package com.notifcatioproject
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class GoesBackround(appContext: Context, workerParams: WorkerParameters) : Worker(appContext, workerParams) {
+
+    override fun doWork(): Result {
+        Log.d(TAG, "Performing long running task in scheduled job")
+        return Result.success()
+    }
+
+    companion object {
+        private val TAG = "MyWorker"
+    }
+}
